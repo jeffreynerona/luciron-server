f50e583f48603b8b1acc0ba44afd7574a1d49e93
jeffreynerona <jeffnerona@gmail.com>
2017-10-03 23:45:34 +0800

    Added annd initialized mongoose

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-
+var mongoose = require('mongoose');
 var port = 3000;
 var app = express();
+
+mongoose.connect('mongodb://localhost/luciron')
+  .then(() =>  console.log('MongoDB Connected'))
+  .catch((err) => console.error(err));
 
 app.use('/', function(req, res){
 res.send("Hello World. Brought to you by: Port "+ port);
